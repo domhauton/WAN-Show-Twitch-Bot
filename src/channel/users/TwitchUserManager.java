57e30953e17783828cbc0e0dc9536950ce410af2
domhauton <domhauton@gmail.com>
2016-03-13 23:16:09 +0000

    Added MessageManager

--- src/channel/users/TwitchUserManager.java
+++ src/channel/users/TwitchUserManager.java
@@ -1,19 +1,36 @@
 package channel.users;
 
+import com.google.common.collect.ImmutableList;
+
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Dominic Hauton on 12/03/2016.
  */
 public class TwitchUserManager {
     private HashMap<String, TwitchUser> userHashMap;
+    private HashMap<UserPermission, ImmutableList> userSnapshot;
 
     public TwitchUserManager() {
         userHashMap = new HashMap<>();
     }
 
     public TwitchUser getUser(String username) {
-        return userHashMap.computeIfAbsent(username, name -> new TwitchUser(name, UserPermission.ChannelUser));
+        return userHashMap.putIfAbsent(username, new TwitchUser(username, UserPermission.ChannelUser) );
+    }
+
+    /**
+     * Get a list of all the users at the given permission level.
+     * @param permission
+     * @return
+     */
+    public ImmutableList getUserSnapshot(UserPermission permission) {
+        List<TwitchUser> filteredUserList = userHashMap.values().stream()
+                .filter(user -> user.getUserPermission().equals(permission))
+                .collect(Collectors.toList());
+        return ImmutableList.copyOf( filteredUserList );
     }
 
     public TwitchUser addUser(TwitchUser twitchUser){
